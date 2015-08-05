04b638c7ff0b47198029f88beeb81f481d8fcba0
pitonov <mustafa.najdiy@gmail.com>
2015-08-05 13:34:25 +0400

    ver 1.1
    add css style in add_vopros.php

--- add_vopros.php
+++ add_vopros.php
@@ -2,6 +2,7 @@
 <head> 
 <title> Добавить вопрос </title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" type="text\css" href="style.css" />
 </head>
 
 
